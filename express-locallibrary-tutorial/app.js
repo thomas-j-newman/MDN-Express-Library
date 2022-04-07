e744b16d4f0a149a6f2190dfaeb0980cfa38578e
DoublyBland <shadowpiedragon@gmail.com>
2022-04-06 19:50:35 -0500

    added controllers and routes

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
 var mongoose = require('mongoose')
 var mongoDB = 'mongodb+srv://user:pass@locallibrary.x7xti.mongodb.net/LocalLibrary?retryWrites=true&w=majority'
-mongoose.connect(mongoDB. {useNewURLParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, {useNewURLParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
